31e46915bb12935a02e49bfbe9a7cfb89c4118dc
natarakonstruksi-bit <natarakonstruksi@gmail.com>
2026-09-12 22:19:57 +0800

    Fix worker export syntax

--- src/worker.js
+++ src/worker.js
@@ -1062,7 +1062,7 @@ return json(
 
 );
 
-} export default { async fetch(request, env, ctx) { const url = new
+export default { async fetch(request, env, ctx) { const url = new
 URL(request.url);
 
     if (url.pathname === "/api/access/session") {
